f8900696deccc6b3d86f7805549edf0b04e04688
eugene <slesh@debian.debian.by>
2015-11-08 17:02:01 +0300

    begin implement building llink-rlink table.

--- network-structure/lw1/script.js
+++ network-structure/lw1/script.js
@@ -24,20 +24,21 @@ window.onload = function () {
     };
 
     function Network(headers) {
-        this.network = [];
+        this.nodes = [];
         this.headers = headers;
     };
 
     Network.prototype.addNode = function (item) {
-        this.network.push(item);
+        this.nodes.push(item);
     };
 
     Network.prototype.fillTable = function (table) {
+        table.innerHTML = "";
         var row = table.insertRow(-1);
         this.headers.forEach(function (header) {
             row.insertCell(-1).innerHTML = header;
         });
-        this.network.forEach(function (item) {
+        this.nodes.forEach(function (item) {
             row = table.insertRow(-1);
             for (var property in item) {
                 if (item.hasOwnProperty(property)) {
@@ -53,7 +54,7 @@ window.onload = function () {
     };
 
     Network.prototype.findAllByProperty = function (property, value) {
-        return this.network.filter(function (node) {
+        return this.nodes.filter(function (node) {
             if (node.hasOwnProperty(property)) {
                 return node[property] == value;
             }
@@ -99,27 +100,68 @@ window.onload = function () {
     };
 
     var network = new Network(["Number", "Id", "Parent", "Left Link", "Right Link"]);
-    network.addNode(new NetworkNode(1,    11,     1,      2,            4));
-    network.addNode(new NetworkNode(2,    111,    11,     -1,           -1));
-    network.addNode(new NetworkNode(3,    12,     2,      5,            14));
-    network.addNode(new NetworkNode(4,    12,     1,      5,             7));
-    network.addNode(new NetworkNode(5,    121,    12,     -1,           6));
-    network.addNode(new NetworkNode(6,    122,    12,     8,            -1));
-    network.addNode(new NetworkNode(7,    13,     1,      12,           -1));
-    network.addNode(new NetworkNode(8,    1221,   122,    -1,           9));
-    network.addNode(new NetworkNode(9,    1222,   122,    10,           -1));
-    network.addNode(new NetworkNode(10,   1228,   1222,   -1,           11));
-    network.addNode(new NetworkNode(11,   1229,   1222,   -1,           -1));
-    network.addNode(new NetworkNode(12,   1221,   13,     -1,           13));
-    network.addNode(new NetworkNode(13,   1222,   13,     10,           -1));
-    network.addNode(new NetworkNode(14,   21,     2,      17,           -1));
-    network.addNode(new NetworkNode(15,   221,    21,     16,           -1));
-    network.addNode(new NetworkNode(16,   1222,   221,    10,           -1));
-    network.addNode(new NetworkNode(17,   13,     21,     12,           15));
+    network.addNode(new NetworkNode(1, 11, 1, 2, 4));
+    network.addNode(new NetworkNode(2, 111, 11, -1, -1));
+    network.addNode(new NetworkNode(3, 12, 2, 5, 14));
+    network.addNode(new NetworkNode(4, 12, 1, 5, 7));
+    network.addNode(new NetworkNode(5, 121, 12, -1, 6));
+    network.addNode(new NetworkNode(6, 122, 12, 8, -1));
+    network.addNode(new NetworkNode(7, 13, 1, 12, -1));
+    network.addNode(new NetworkNode(8, 1221, 122, -1, 9));
+    network.addNode(new NetworkNode(9, 1222, 122, 10, -1));
+    network.addNode(new NetworkNode(10, 1228, 1222, -1, 11));
+    network.addNode(new NetworkNode(11, 1229, 1222, -1, -1));
+    network.addNode(new NetworkNode(12, 1221, 13, -1, 13));
+    network.addNode(new NetworkNode(13, 1222, 13, 10, -1));
+    network.addNode(new NetworkNode(14, 21, 2, 17, -1));
+    network.addNode(new NetworkNode(15, 221, 21, 16, -1));
+    network.addNode(new NetworkNode(16, 1222, 221, 10, -1));
+    network.addNode(new NetworkNode(17, 13, 21, 12, 15));
     network.fillTable($("#source-table"));
 
     $("#search-button").addEventListener("click", function () {
         $(".output").innerHTML = network.findPath($("#start-node").value);
+    });
+
+    $("#clear-button").addEventListener("click", function () {
+        network.nodes.forEach(function (node) {
+            node.leftLink = node.rightLink = "";
+        });
+        network.fillTable($("#source-table"));
+    });
+
+    $("#build-button").addEventListener("click", function () {
+        var used = [];
+        var temp = null;
+        var parentId = 1;
+        var ps = network.findAllByProperty("parent", parentId);
+        var bundles = [];
+        ps.forEach(function () {
+
+        });
+        network.nodes.forEach(function (node) {
+            var parents = network.findAllByProperty("parent", node.parent);
+            var children = network.findAllByProperty("parent", node.id);
+            if(children.length >= 1){
+                node.leftLink = children[0].number;
+            }else{
+                node.leftLink = -1;
+            }
+            node.rightLink = -1;
+            parents.forEach(function (parent, index) {
+                if(index == 0){
+                    temp = parent;
+                    return;
+                }
+                temp.rightLink = parent.number;
+                temp = parent;
+                if (used.indexOf(temp.number) != -1) {
+                    temp.rightLink = -1;
+                }
+                used.push(temp.number);
+            });
+        });
+        network.fillTable($("#source-table"));
     });
 };
 
